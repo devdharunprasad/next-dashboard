7e7ef74fbc7ce5af74f06e8377524e906854f5eb
devdharunprasad <dharun@vizdale.com>
2023-10-28 17:00:18 +0530

    Initial commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Here are your invoices</div>
+  )
+}
+
+export default Page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+ const Page = () => {
+    return(
+        <h1>Welcome to dashboard</h1>
+    )
+}
+export default Page
